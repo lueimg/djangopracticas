e90492bc88ba5b95cbf0db821fce2295ad38822e
luis mori <luismorith@gmail.com>
2014-07-02 01:45:01 -0500

    + funciones en el administrador basico

--- tracks/admin.py
+++ tracks/admin.py
@@ -7,6 +7,17 @@ from .models import Track
 class TrackAdmin(admin.ModelAdmin):
 	#aumenta columnas en el admin de este contenido
 	list_display = ('title','artist','order', 'track_file','album')
+	
+	#para filtar en el admin por tipo de contenido
+	list_filter = ('artist','album')
+
+	#AGREGA UN BUSCADOR A LA CLASE
+	search_fields = ('title','artist__firt_name','artist__last_name')
+
+
+
+
+
 
 # agrega el modelo y la clase de configuracion
 admin.site.register(Track , TrackAdmin)
