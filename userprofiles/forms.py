e5caeb5cd4a8e4219f655e98770c592ca20c8fcf
luis mori <luismorith@gmail.com>
2014-07-02 00:58:22 -0500

    registrando y logenado a usuarios

--- userprofiles/forms.py
+++ userprofiles/forms.py
@@ -1,10 +1,38 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
+#FORMULARIO DE REGISTRO
 class UserCreationEmailForm(UserCreationForm):
 	email = forms.EmailField()
 
 	class Meta:
 		model = User
 		fields = ("username","email",)
+
+#FORMULARIO DE LOGIN
+class EmailAuthenticationForm(forms.Form):
+	email = forms.EmailField()
+	password = forms.CharField(label='password',widget=forms.PasswordInput)
+
+	def __init__(self,*args,**kwargs):
+		self.user_cache = None
+		super(EmailAuthenticationForm, self).__init__(*args,**kwargs)
+
+	#VALIDACIONES
+	def clean(self):
+		email = self.cleaned_data.get('email')
+		password = self.cleaned_data.get('password')
+
+		self.user_cache = authenticate(email=email,password=password)
+
+		if self.user_cache is None:
+			raise forms.ValidationError('Usuario incorrecto')
+		elif not self.user_cache.is_active:
+			raise forms.ValidationError('Usuario inactivo')
+		# si todo esta ok el formulario pasa todo correctamente
+		return self.cleaned_data
+
+	def get_user(self):
+		return self.user_cache
--- userprofiles/views.py
+++ userprofiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .forms import UserCreationEmailForm
+from .forms import UserCreationEmailForm, EmailAuthenticationForm
+from django.contrib.auth import login
 
 def signup(request):
 	# request.POST or None valida si se esta recibiendo post para llenar el formualrio o no 
@@ -9,3 +10,13 @@ def signup(request):
 		form.save()
 
 	return render(request, "signup.html",{"form":form})
+
+def signin(request):
+	form = EmailAuthenticationForm(request.POST or None)
+
+	if form.is_valid():
+		
+		login(request, form.get_user())
+
+	return render(request, 'signin.html', {'form':form})
+
