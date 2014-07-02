79d9cfba37d76922fa7a01078598693aaf4d2f4b
luis mori <luismorith@gmail.com>
2014-07-02 11:23:59 -0500

    Admin , edit , inline otros modelos

--- artists/admin.py
+++ artists/admin.py
@@ -3,10 +3,27 @@ from django.contrib import admin
 # Register your models here.
 from .models import Artist
 
+#vamos a editar los tracks desde el modelo artist
+from tracks.models import Track
+
+## para manejar edicion de datos relaciones e nla misma liena
+class TrackInline(admin.StackedInline):
+	model = Track
+	extra = 1 # campos vacios extras para agregar mas tracks  a los artistas
+
+from albums.models import Album
+
+class AlbumInline(admin.StackedInline):
+	model = Album
+	extra = 1
+
 
 class ArtistAdmin(admin.ModelAdmin):
 
 	search_fields = ('firt_name','last_name')
+
+	inlines = [AlbumInline,TrackInline,] #clases de modelos a relacionar para editar 
+
 
 
 
